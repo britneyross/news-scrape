4d7f514ec5c3eeefb490b3f9f37002593175b957
Britney Ross <britneyross25@yahoo.com>
2020-06-14 21:49:14 -0500

    added background image

---
+++ public/main.js
@@ -0,0 +1,17 @@
+// let data = {"saved":true}
+
+// user_id = get data-id value 
+
+// event listener to the saved button 
+// $.ajax({
+//     type: 'PUT',
+//     url: '/saved/user_id',
+//     contentType: 'application/json',
+//     data: JSON.stringify(data), // access in body
+// }).done(function () {
+//     console.log('SUCCESS');
+// }).fail(function (msg) {
+//     console.log('FAIL');
+// }).always(function (msg) {
+//     console.log('ALWAYS');
+// });
--- routes/route.js
+++ routes/route.js
@@ -9,49 +9,49 @@ const app = express();
 const db = require("../models");
 
 app.get("/", (req, res) => {
-    db.Article.find({})
-        .lean()
-        .then(function (data) {
-            res.render("index", { articles: data });
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
+  db.Article.find({})
+    .lean()
+    .then(function (data) {
+      res.render("index", { articles: data });
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
 // A GET route for scraping the Democracy Now website
 app.get("/scrape", function (req, res) {
-    // First, we grab the body of the html with axios
-    axios.get("https://www.democracynow.org/").then(function (response) {
-        // Then, we load that into cheerio and save it to $ for a shorthand selector
-        var $ = cheerio.load(response.data);
+  // First, we grab the body of the html with axios
+  axios.get("https://www.democracynow.org/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
-        $(".news_item h3").each(function (i, element) {
-            // Save an empty result object
-            var result = {};
+    $(".news_item h3").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
 
-            // Add the text and href of every link, and save them as properties of the result object
-            result.title = $(this).children("a").text();
-            result.link = $(this).children("a").attr("href");
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
 
-            // Create a new Article using the `result` object built from scraping
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    // View the added result in the console
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    // If an error occurred, log it
-                    console.log(err);
-                });
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, log it
+          console.log(err);
         });
-        res.redirect("/");
     });
+    res.redirect("/");
+  });
 });
 
 // Clear the DB
-app.get("/clearall", function(req, res) {
-  db.Article.remove({}, function(error, response) {
+app.get("/clearall", function (req, res) {
+  db.Article.remove({}, function (error, response) {
     // Log any errors to the console
     if (error) {
       console.log(error);
@@ -66,18 +66,38 @@ app.get("/clearall", function(req, res) {
   });
 });
 
-app.get("/saved", function(req,  res){
+app.get("/saved", function (req, res) {
   db.Article.find({})
-  .where('saved').equals(true)
-  .where('deleted').equals(false)
-  .exec(function(err, articles){
-    if(err){
-      console.log(err);
-    } else {
-      console.log(articles);
-    };
-    res.render("saved");
-  });
+    .where('saved').equals(true)
+    .exec(function (err, article) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(article);
+      };
+      res.render("saved");
+    });
 });
 
-module.exports = app;
+app.put("/submit/:id", function (req, res) {
+  // Create a new article in the database
+  /*db.Saved.create(req.body)
+    .then(function(dbSaved) {
+      return db.Article.findOneAndUpdate({}, { $push: { articles: dbSaved._id } }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });*/
+  db.Article.update({ _id: req.params.id }, { saved: true }, function (err, raw) {
+    if (err) {
+      res.send(err);
+    }
+    res.send(raw);
+
+  });
+  });
+
+  module.exports = app;
